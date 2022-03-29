2505cbdff8b964d146f963a4115463a896e3caca
Ruslan <rnzamuraev@gmail.com>
2022-03-29 17:20:54 +0300

    big changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// scroll
+
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
@@ -12,3 +14,61 @@ for (let anchor of anchors) {
     });
   });
 }
+
+// carousel
+
+const prev = document.getElementById("btn-prev"),
+  next = document.getElementById("btn-next"),
+  slides = document.querySelectorAll(".carousel__slide"),
+  dots = document.querySelectorAll(".carousel__dot");
+
+let index = 0;
+
+const activeSlide = (n) => {
+  for (slide of slides) {
+    slide.classList.remove("active");
+  }
+  slides[n].classList.add("active");
+};
+
+const activeDot = (n) => {
+  for (dot of dots) {
+    dot.classList.remove("active");
+  }
+  dots[n].classList.add("active");
+};
+
+const preepareCurrentSlide = (ind) => {
+  activeSlide(index);
+  activeDot(index);
+};
+
+const nextSlide = () => {
+  if (index == slides.length - 1) {
+    index = 0;
+    preepareCurrentSlide(index);
+  } else {
+    index++;
+    preepareCurrentSlide(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index == 0) {
+    index = slides.length - 1;
+    preepareCurrentSlide(index);
+  } else {
+    index--;
+    preepareCurrentSlide(index);
+  }
+};
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener("click", () => {
+    index = indexDot;
+    preepareCurrentSlide(index);
+  });
+});
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
